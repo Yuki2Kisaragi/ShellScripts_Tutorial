f40c3f25f77744f17dfc71d2c4d53de1d0ba3ab6
Yuki2Kisaragi <masahero.yuffie@outlook.jp>
2020-05-02 09:58:57 +0900

    WorkManagement.sh Modified

--- Products/WorkAssistant/WorkManagement.sh
+++ Products/WorkAssistant/WorkManagement.sh
@@ -1,25 +1,33 @@
-#! /bin/sh
+#! /bin/bash
 
 
 TODAY=`date +"%Y%m%d"`
-
 WORK_START_TIME=`date +"%H:%M"`
 
+if [ -e Output/WorkLog_$TODAY.txt ]; then
+    :
+    echo " 'WorkLog_$TODAY.txt' already exists."
+else
 
-echo "Log Start Time : $WORK_START_TIME" > WorkLog_$TODAY.txt
+    echo "Log Start Time : $WORK_START_TIME" > Output/WorkLog_$TODAY.txt
 
-echo "Please Tell Me , your scheduled attendance time."
-echo "Enter like HH:MM "
-echo -n Start:
-read START_TIME
+    echo "Please Tell Me , your scheduled attendance time."
+    echo "Enter like HH:MM "
+    echo -n 'Start TIME = '
+    read START_TIME
 
-echo -n End:
-read END_TIME
+    echo -n 'End   TIME = ' 
+    read END_TIME
 
-echo "-------------------------------------"    >> WorkLog_$TODAY.txt
-echo "Start TIME : $START_TIME"                 >> WorkLog_$TODAY.txt
-echo "End   TIME : $END_TIME"                   >> WorkLog_$TODAY.txt
-echo "-------------------------------------"    >> WorkLog_$TODAY.txt
+    echo "-------------------------------------"    >> Output/WorkLog_$TODAY.txt
+    echo "Start TIME : $START_TIME"                 >> Output/WorkLog_$TODAY.txt
+    echo "End   TIME : $END_TIME"                   >> Output/WorkLog_$TODAY.txt
+    echo "-------------------------------------"    >> Output/WorkLog_$TODAY.txt
 
-echo "Please your Task!"
-echo "-------------------------------------"    >> WorkLog_$TODAY.txt
+    echo "Please your Task!"
+    echo "-------------------------------------"    >> Output/WorkLog_$TODAY.txt
+
+    echo "-------------------------------------"    >> Output/WorkLog_$TODAY.txt
+
+    `chmod 444 Output/WorkLog_$TODAY.txt `
+fi
